15baabf527dfdad0fe5188c86e4e51c8ab8d4d9a
Mperwe <frankmperwe@gmail.com>
2025-10-25 03:38:06 +0200

    edit home page

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/admin/login" element={<AdminLogin onLogin={handleLogin} />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard token={adminToken} setToken={setAdminToken} />} />
+        <Route path="/admin/dashboo
+        ard" element={<AdminDashboard token={adminToken} setToken={setAdminToken} />} />
       </Routes>
     </Router>
   );
